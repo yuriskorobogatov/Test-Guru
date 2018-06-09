b8ea29665897d0d77f95eea7db36ba2a1fafe1d5
yuriskorobogatov <yuriskorobogatov1991@gmail.com>
2018-06-09 11:09:25 +0500

    first commit

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,9 @@ module TestGuru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Ekaterinburg'
+    I18n.available_locales = %i[ru en]
+    I18n.default_locale = :ru
+    config.encoding = 'utf-8'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  root 'welcome#index'
+
+  get 'welcome/about'
+  get 'welcome/about/author', to: 'welcome#author'
+  resources :welcome
 end
