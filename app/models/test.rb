bd91e82f1aa0682af6d78b5fa33cab6e91bcf0b8
yuriskorobogatov <yuriskorobogatov1991@gmail.com>
2019-04-15 16:30:43 +0500

    associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,10 @@ class Test < ApplicationRecord
 
   has_many :users, through: :user_test_throughs
   has_many :user_test_throughs
+  has_many :questions
+  
+
+  
 
   def self.show_tests(category)
     Test.joins(' JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
     has_many :tests, through: :user_test_throughs
     has_many :user_test_throughs
-
+    
 
     def test_list (level)
         Test.joins('JOIN user_test_throughs ON tests.id = user_test_throughs.test_id')
