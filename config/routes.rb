cd0e3b9323523c23d5556a7eaa7bd4edca17c400
yuriskorobogatov <yuriskorobogatov1991@gmail.com>
2019-05-24 15:52:22 +0500

    flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
  
+  get 'sessions/new'
+
   root 'tests#index'
   
   get 'users/new'
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
     resources :tests do
         resources :questions, shallow: true, except: :index do
