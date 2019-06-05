563b57b444483a500cee504dcb843c0a570fb540
yuriskorobogatov <yuriskorobogatov1991@gmail.com>
2019-06-05 17:42:14 +0500

    first commit of locales

--- Gemfile
+++ Gemfile
@@ -29,9 +29,17 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+
+#find errors
 gem 'pry'
 
+#authorization
 gem 'devise'
+
+#localization
+gem 'rails-i18n'
+
+
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
 
    def after_sign_in_path_for(resourse)
     if resourse.admin? 
@@ -8,6 +10,16 @@ class ApplicationController < ActionController::Base
     else
       tests_path
     end
+  end
+
+  def default_url_options
+    {lang: I18n.locale}
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale 
   end
 
  protected
