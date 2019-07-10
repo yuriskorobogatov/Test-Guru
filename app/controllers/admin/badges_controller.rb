7e201e4842715904a729372ca2b785aeade0776b
yuriskorobogatov <yuriskorobogatov1991@gmail.com>
2019-07-10 14:28:50 +0500

    delete badge

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,22 +1,26 @@
 class Admin::BadgesController < Admin::BaseController
     before_action :authenticate_user!
+    before_action :find_badge, only: %i[show edit update destroy]
 
   def index
     @badges = Badge.all
   end
 
   def show
-    @badge = Badge.find(params[:id])
+
   end
 
   def new
     @badge = Badge.new
   end
 
+  def edit
+
+  end
+
   def create
     @badge = Badge.new(badge_params)
     @badge.author = current_user
-
     if @badge.save
         redirect_to [:admin, @badge], notice: 'Badge was create successfuilly'
     else
@@ -24,13 +28,26 @@ class Admin::BadgesController < Admin::BaseController
     end
   end
 
+  def update
+    if @badge.update(badge_params)
+    redirect_to admin_badge_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @badge.destroy
+    redirect_to admin_badges_path
   end
 
   private
     def badge_params
         params.require(:badge).permit(:title, :reward_link, :rule_id)
+    end
+
+    def find_badge
+      @badge = Badge.find(params[:id])
     end
 
 end
